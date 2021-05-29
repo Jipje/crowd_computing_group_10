10eafd5040266f09568b541120de3f3afb141476
Bert <b.konuralp@student.tudelft.nl>
2021-05-29 21:57:51 +0200

    groups, customization phases, cleanup, comments

--- Avatar Personalization/Toloka_Avatar/tolokaAvatarPersonalization.js
+++ Avatar Personalization/Toloka_Avatar/tolokaAvatarPersonalization.js
@@ -1,6 +1,10 @@
+const avatarStyles = [
+    'Transparent', 'Circle'
+]
+
 const topTypes = [
     'NoHair', 
-    'EyePatch', 
+    'Eyepatch', 
     'Hat', 
     'Hijab',
     'Turban',
@@ -184,7 +188,7 @@ const skinColors = [
 const types = {
     avatarStyles: {
         name: "Background",
-        options: ['Transparent', 'Circle'],
+        options: avatarStyles,
         selection: "Transparent"
     },
     skinColors: {
@@ -249,69 +253,121 @@ const types = {
     }
 }
 
+const groupFourOptions = {
+    phaseOne: ['avatarStyles', 'skinColors'],
+    phaseTwo: ['topTypes', 'hairColors', 'hatColors'],
+    phaseThree: ['eyeTypes'],
+    phaseFour: ['mouthTypes'],
+    phaseFive: ['eyebrowTypes'],
+    phaseSix: ['clotheTypes', 'clotheColors'],
+    phaseSeven: ['facialHairTypes', 'facialHairColors', 'accessoriesTypes']
+}
+
+let currentPhase = 1;
+let avatar_name = "";
+
 exports.Task = extend(TolokaHandlebarsTask, function (options) {
   TolokaHandlebarsTask.call(this, options);
 }, {
-    // DOM element for task is formed (available via #getDOMElement())
+    // Called when a task is to be created
     onRender: function() {
-        loadAvatar(this.getDOMElement());
+        // Get relevant data from DOM and append the current phase for this task inside the DOM so it can be used later
+        let isTaskElement = this.getDOMElement().querySelector("#isTask");
+        let groupElement = this.getDOMElement().querySelector("#group");
+        let avatarElement = this.getDOMElement().querySelector("#avatarImage");
+        this.getDOMElement().querySelector("#phase").innerHTML = currentPhase;
 
-        if (this.getDOMElement().querySelector("#isTask").innerText !== "true") {
+        // Load avatar only if group number isn't 1
+        if (groupElement.innerHTML !== '1') loadAvatar(this.getDOMElement());
+        else avatarElement.remove();
+
+        // Is avatar customization, so remove the task div and load options, and increment currentPhase
+        if (isTaskElement.innerText !== "true") {
             this.getDOMElement().querySelector("#task").remove();
             loadOptions(this.getDOMElement());
+            if (currentPhase > 1) this.getDOMElement().querySelector("#avatar_name_input").remove();
+            currentPhase++;
+
+        // Is a task, remove avatar name input area and display avatar name on top
+        } else {
+            this.getDOMElement().querySelector("#avatar_name_input").remove();
+            this.getDOMElement().querySelector("#avatar_name").innerHTML = avatar_name;
         }
 
-        this.getDOMElement().querySelector("#isTask").remove();
+        // Hide isTask and group number that is stored in the DOM
+        isTaskElement.style.display = 'none';
+        groupElement.style.display = 'none';
     },
-    // Task is completed. Global resources can be released (if used)
-    onDestroy: function() {
-        
-    },
-    // Task is in focus, reload the avatar
+
+    // Task is in focus, reload the avatar and show avatar name if group for the task isn't 1
     onFocus: function() {
-        loadAvatar(this.getDOMElement());
+        let groupElement = this.getDOMElement().querySelector("#group");
+        if (groupElement.innerHTML !== '1') {
+            loadAvatar(this.getDOMElement());
+            this.getDOMElement().querySelector("#avatar_name").innerHTML = avatar_name;
+        }
+    },
+
+    // Task is out of focus, if this is first task, get the answer for the 'middle name' output (hidden as avatar name) and update global avatar name
+    onBlur: function() {
+        if (this.getSolution().task_id === "0" ) avatar_name = this.getSolution().output_values.middle_name;
     }
 });
 
+// Load the avatar options
 function loadOptions(dom) {
+    // Get needed data from DOM
     let optionsDiv = dom.querySelector("#types");
+    let groupElement = dom.querySelector("#group").innerHTML;
+    let phase = dom.querySelector("#phase").innerHTML;
 
+    // Loop through all types of options
     for (const [key, type] of Object.entries(types)) {
-        let div = document.createElement("div");
-        div.setAttribute("id", "avatar_option")
-        optionsDiv.appendChild(div);
-
-        let labelElement = document.createElement("label");
-        labelElement.for = type.name;
-        labelElement.innerText = type.name + ': ';
-
-        div.appendChild(labelElement);
         
-        let selectElement = document.createElement("select");
-        selectElement.addEventListener("change", function() {
-            for (const [key, type] of Object.entries(types)) {
-                types[key].selection = dom.querySelector("#" + key).value;
+        // Is the group number 4? --> Does the current phase include this option? --> Go on and load it
+        // Is the group number 3? --> Go on and load it (so, all options)
+        if ((getCurrentPhaseOptions(phase).includes(key)  && groupElement === '4') || groupElement === '3') {
+            
+            // Create a new div that will store the new option and append it to the div that contains the options
+            let div = document.createElement("div");
+            div.setAttribute("id", "avatar_option");
+            optionsDiv.appendChild(div);
+
+            // Create the label for the new option and append it to newly created div
+            let labelElement = document.createElement("label");
+            labelElement.for = type.name;
+            labelElement.innerText = type.name + ': ';
+            div.appendChild(labelElement);
+            
+            // Create the select element and add a 'change' event listener to it
+            let selectElement = document.createElement("select");
+            selectElement.addEventListener("change", function() {
+                // Update selection
+                types[this.name].selection = this.value;
+
+                // Load new avatar
+                loadAvatar(dom);
+            });
+
+            selectElement.setAttribute("name", key);
+
+            // Loop through all the options of the current type and append it as an option to the current select element
+            for (const option of Object.values(type.options)) {
+                let optionElement = document.createElement("option");
+                optionElement.text = option;
+                // Set the default options as selected
+                if (option === type.selection)  optionElement.setAttribute("selected", "selected");
+                selectElement.add(optionElement);
             }
-            loadAvatar(dom);
-        });
-
-        selectElement.setAttribute("name", key);
-        selectElement.id = key;
-
-        for (const option of Object.values(type.options)) {
-            let optionElement = document.createElement("option");
-            optionElement.text = option;
-            if (option === type.selection)  optionElement.setAttribute("selected", "selected");
-            selectElement.add(optionElement);
+            div.appendChild(selectElement);
         }
-        div.appendChild(selectElement);
     }
-    
 }
 
 function loadAvatar(dom) {
     let url='https://avataaars.io/?';
 
+    // Append the latest options to the url
     url = url +
     'avatarStyle='      + types.avatarStyles.selection        + '&' +
     'topType='          + types.topTypes.selection            + '&' +
@@ -327,8 +383,21 @@ function loadAvatar(dom) {
     'mouthType='        + types.mouthTypes.selection          + '&' +
     'skinColor='        + types.skinColors.selection          + '&';
 
-    // Append avatar based on options
+    // Append avatar with url to dom
     dom.querySelector("#avatarImage").innerHTML = "<img id='avatar' src='" + url + "' />";
+}
+
+// Given a phase number, get a list of options
+function getCurrentPhaseOptions(phase) {
+    switch(parseInt(phase)) {
+        case 1: return groupFourOptions.phaseOne;
+        case 2: return groupFourOptions.phaseTwo;
+        case 3: return groupFourOptions.phaseThree;
+        case 4: return groupFourOptions.phaseFour;
+        case 5: return groupFourOptions.phaseFive;
+        case 6: return groupFourOptions.phaseSix;
+        case 7: return groupFourOptions.phaseSeven;
+    }
 }
 
 function extend(ParentClass, constructorFunction, prototypeHash) {
